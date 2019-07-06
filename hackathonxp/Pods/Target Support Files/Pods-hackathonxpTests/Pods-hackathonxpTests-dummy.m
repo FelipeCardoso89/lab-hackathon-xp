f96257b6482d9605c7a46d19524799c71d0fd1ca
Felipe Cardoso <felipe.cardoso@nextel.com.br>
2019-07-05 21:59:32 -0300

    first commit

---
+++ hackathonxp/Pods/Target Support Files/Pods-hackathonxpTests/Pods-hackathonxpTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hackathonxpTests : NSObject
+@end
+@implementation PodsDummy_Pods_hackathonxpTests
+@end
---
+++ hackathonxp/Pods/Target Support Files/Pods-hackathonxpUITests/Pods-hackathonxpUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hackathonxpUITests : NSObject
+@end
+@implementation PodsDummy_Pods_hackathonxpUITests
+@end
